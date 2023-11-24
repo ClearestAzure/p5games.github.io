37a56636dc37316bd6c05479900754313c4e54e4
ClearestSkies <149781675+ClearestSkies@users.noreply.github.com>
2023-11-24 10:35:50 +0100

    ex 1

--- JS/H1/H1O01.js
+++ JS/H1/H1O01.js
@@ -1,6 +1,6 @@
 function setup() {
-  canvas = createCanvas(1000,500);
-  background('yellow');
+  canvas = createCanvas(450,450);
+  background('grey');
   canvas.parent('processing');
   noLoop();
 }
@@ -9,13 +9,12 @@ function draw() {
   // groene cirkel zonder rand
   
   noStroke();
-  fill('red');
-  ellipse(300,200,400);
-  
-  // witte rechthoek met rode rand
-  
-  stroke('red');
-  fill('white');
-  strokeWeight(10);
-  rect(650,100,250,300);
+  fill('green');
+  ellipse(225,225,425);
+   fill("white");
+   ellipse(225,225,300);
+   stroke('green');
+   fill('white');
+   strokeWeight(10);
+   rect(125,125,200,200);
 }
