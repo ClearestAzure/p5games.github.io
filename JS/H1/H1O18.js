42c553007cfe3af28d0f42e7f44e3b184e67ec6d
Azure <149781675+ClearestAzure@users.noreply.github.com>
2024-03-08 08:45:08 +0100

    op 18 finished

--- JS/H1/H1O18.js
+++ JS/H1/H1O18.js
@@ -11,15 +11,16 @@ function setup() {
 }
 
 function draw() {
-  //background(0,0,75,0.1);
+  background(0,0,75,0.1);
   tekenSter(xPositie,yPositie);
   yPositie += 1;
   xPositie += 5;
+  schaal += 0.05
 }
 
 function tekenSter(x,y) {
   push();
-  scale(1);
+  scale(schaal);
   strokeWeight(2);
   stroke('white');
   fill('khaki');
