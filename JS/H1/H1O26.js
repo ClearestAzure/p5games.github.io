61ff6f27ed77625e66310d6c82dfc1c3ba5a6d51
ClearestSkies <149781675+ClearestSkies@users.noreply.github.com>
2024-03-17 12:37:45 +0100

    Op 26 finished

--- JS/H1/H1O26.js
+++ JS/H1/H1O26.js
@@ -9,12 +9,12 @@ function draw() {
   background('orange');
   fill('wheat');
   rect(0,250,width,height - 250);
-  translate(125,0);
+  //translate(125,0);
 
-  for (var n = 1;n <= 4;n++) {
+  for (var n = 0;n <=5 ;n++) {
     tekenHuis();
     tekenBoom(n);
-    translate(200,0);
+    translate(165,0);
   }
 }
 
@@ -23,11 +23,11 @@ function tekenHuis() {
   strokeWeight(4);
   stroke('darkgrey');
   fill('lightgray');
-  rect(0,160,100,100);
+  rect(10,170,110,110);
   fill('gray');
-  triangle(0,160,100,160,50,80);
+  triangle(10,170,110,170,60,90);
   noStroke();
-  rect(20,210,30,50);
+  rect(30,220,40,60);
   pop();
 }
 
@@ -35,8 +35,8 @@ function tekenBoom(x) {
   push();
   noStroke();
   fill('sienna');
-  rect(80,150,40,130);
+  rect(90,160,40,130);
   fill('olive');
-  ellipse(100,170 - 15*x,60 + 10*x,100 + 20*x);
+  ellipse(110,185 - 15*x,60 + 10*x,100 + 20*x);
   pop();
 }
