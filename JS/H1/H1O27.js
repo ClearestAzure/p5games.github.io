12bbeae15bdfd474892ac8c66ebe3f03067cf97d
Azure <149781675+ClearestAzure@users.noreply.github.com>
2024-04-05 09:16:09 +0200

    op 27 finished

--- JS/H1/H1O27.js
+++ JS/H1/H1O27.js
@@ -1,6 +1,7 @@
 var aantal = 4;
 var breedte;
 
+
 function setup() {
   canvas = createCanvas(1000,300);
   canvas.parent('processing');
@@ -13,14 +14,14 @@ function setup() {
 function draw() {
   background('lavender');
   fill('black');
-  breedte = width / 4;
+  breedte = 1000/aantal;
   text("aantal = " + aantal,10,20);  
   
   push();
   translate(0.5*breedte,150);
 
-  for (var n = 0; n < 4;n++) {
-    tekenJos(2);
+  for (var n = 0; n < aantal;n++) {
+    tekenJos(2+n/2);
     translate(breedte,0);
   }
   
@@ -29,6 +30,12 @@ function draw() {
   if (keyIsDown(RIGHT_ARROW)) {
     aantal++;
   }   
+  if (keyIsDown(LEFT_ARROW) ){
+    aantal--;
+  }
+  if (aantal < 1 ){
+    aantal = 1 ;
+  }
 }
 
 function tekenJos(s) {
