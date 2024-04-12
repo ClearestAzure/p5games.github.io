f7ddde4dd90a639ffdc53e754e2e805287fb808c
Azure <149781675+ClearestAzure@users.noreply.github.com>
2024-04-12 11:36:29 +0200

    op 30 31 finished

--- JS/H1/H1O30.js
+++ JS/H1/H1O30.js
@@ -8,7 +8,8 @@ function setup() {
 }
 
 function draw() {
-  for( var rij = 0;  rij < 450; rij +=50 ){
+  fill('white');
+  for ( var rij = 0;  rij < 450; rij +=50 ){
     for (var kolom = 0;kolom < 450;kolom += 50) {
       if (rij == 150 && kolom == 300){
         fill('orange');
--- JS/H1/H1O31.js
+++ JS/H1/H1O31.js
@@ -1,15 +1,19 @@
 kleur='white';
 
 function setup() {
-  canvas = createCanvas(451,451);
+  canvas = createCanvas(501,501);
   canvas.parent('processing');
   background('coral');
 }
 
 function draw() {
-  for (var rij = 0;rij < 450;rij += 50) {
-    for (var kolom = 0;kolom < 450;kolom += 50) {
-      fill(kleur);
+  for (var rij = 0;rij < 500;rij += 50) {
+    for (var kolom = 0;kolom < 500;kolom += 50) {
+      if ((rij + kolom) % 100 === 0) {
+        fill('kleur');
+      } else {
+        fill('black');
+      }
       rect(kolom,rij,50,50);
     }
   }
