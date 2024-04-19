ca480f0d858d96fd4d091cf15a12731191df060d
ClearestSkies <149781675+ClearestSkies@users.noreply.github.com>
2024-04-19 18:24:54 +0200

    op 34

--- JS/H1/H1O34.js
+++ JS/H1/H1O34.js
@@ -1,18 +1,48 @@
-var aantal = 100;
-var x;
-var y;
-var diameter;
+var aantal = 100; 
+var x; 
+var y; 
+var diameter; 
 
-function setup() {
-  canvas = createCanvas(451,451);
-  canvas.parent('processing');
-  frameRate(5);
-  noLoop();
-  strokeWeight(4);
-  stroke('steelblue');
-}
+ 
 
-function draw() {
-  background('white');
+function setup() { 
 
-}
+canvas = createCanvas(451,451); 
+
+canvas.parent('processing'); 
+
+frameRate(5); 
+
+// noLoop(); 
+
+strokeWeight(4); 
+
+stroke('steelblue'); 
+
+} 
+
+ 
+
+function draw() { 
+
+background('white'); 
+
+for (var n = 0; n<aantal; n++) { 
+
+ 
+
+x = random (50,400) 
+
+y = random (50, 400) 
+
+diameter = random(25,75) 
+
+drawcircle()}; 
+
+} 
+
+function drawcircle () { 
+
+circle(x,y,diameter); 
+
+} 
