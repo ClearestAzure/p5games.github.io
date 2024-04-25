82631d58110f09212f82ed5c7810de6f81cf2033
ClearestSkies <149781675+ClearestSkies@users.noreply.github.com>
2024-04-25 20:12:41 +0200

    29 and 34

--- JS/H1/H1O34.js
+++ JS/H1/H1O34.js
@@ -3,26 +3,15 @@ var x;
 var y; 
 var diameter; 
 
- 
-
 function setup() { 
-
 canvas = createCanvas(451,451); 
-
 canvas.parent('processing'); 
-
 frameRate(5); 
-
 // noLoop(); 
 
 strokeWeight(4); 
-
 stroke('steelblue'); 
-
 } 
-
- 
-
 function draw() { 
 
 background('white'); 
